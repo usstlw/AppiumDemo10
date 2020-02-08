c63aee45da83ffaa6af02fc37c78eb2625d770f4
usstlw <usstlw@126.com>
2020-02-08 11:08:02 +0800

    update

--- testcase/test_calc.py
+++ testcase/test_calc.py
@@ -1,9 +1,13 @@
 from unittest import TestCase
 from ddt import ddt,data,file_data,unpack
+import yaml
 from src.calc import Calc
 
 @ddt
 class TestCalc(TestCase):
+
+    def setUpClass(cls) -> None:
+        self.calc=Calc()
     @data(1,2,3,4,5,6)
     def test_demo(self,real):
         self.assertEqual(real,5)
@@ -15,19 +19,22 @@ class TestCalc(TestCase):
           )
     @unpack
     def test_add(self,a,b,c):
-        calc=Calc()
-        result=calc.add(a,b)
+        print(a,b,c)
+        result=self.calc.add(a,b)
+        # print("result="+str(result))
         # assert  result == 2
-        self.assertEqual(result,c)
+        print(a,b,c)
+        self.assertEqual(self.calc.add(a,b),c)
 
 
-    def test_div(self):
+    @file_data("calc.yaml")
+    def test_div(self,a,b,c):
         calc=Calc()
-        result=calc.div(2,1)
-        self.assertEqual(result,2)
+        print(a,b,c)
+        self.assertEqual(calc.div(a,b),c)
 
-        result=calc.div(1,2)
-        self.assertEqual(result,0.5)
-
-        result=calc.div(100,100)
-        self.assertEqual(result,1)
+        # result=calc.div(1,2)
+        # self.assertEqual(result,0.5)
+        #
+        # result=calc.div(100,100)
+        # self.assertEqual(result,1)
